wrong results for CR - index2 and index1

comp_timeschemes_main.py
@@ -241,8 +241,8 @@ class UpFiles(object):
 
 if __name__ == '__main__':
     scheme = 'CR'
-    # import dolfin_navier_scipy.data_output_utils as dou
-    # dou.logtofile(logstr='logfile3')
+    import dolfin_navier_scipy.data_output_utils as dou
+    dou.logtofile(logstr='logfile3')
     # solve_euler_timedep(method=2, N=20, tE=1.0, LinaTol=0,  # 2**(-12),
     #                       MaxIter=85, NtsList=[16, 23, 32])
     # , 45, 64,91, 128])
@@ -252,11 +252,11 @@ if __name__ == '__main__':
     # solve_euler_timedep(method=1, N=80, NtsList=[32])
     # solve_euler_timedep(method=1, N=80, NtsList=[64])
     # solve_euler_timedep(method=1, N=20, NtsList=[16])
-    solve_euler_timedep(method=1, N=60, LinaTol=2**(-10),
-                        MaxIter=100, NtsList=[16, 64, 256, 1024],
-                        scheme=scheme)
-    # solve_euler_timedep(method=1, N=60, LinaTol=0,
-    #                     MaxIter=100, NtsList=[16],  # , 64, 256, 1024],
+    # solve_euler_timedep(method=1, N=50, LinaTol=2**(-10),
+    #                     MaxIter=200, NtsList=[16, 64, 256, 1024],
     #                     scheme=scheme)
+    solve_euler_timedep(method=2, N=40, LinaTol=0,
+                        MaxIter=100, NtsList=[16, 32, 64, 128],
+                        scheme=scheme)
     # solve_euler_timedep(method=1, N=80, NtsList=[32])
     # solve_euler_timedep(method=1, N=80, NtsList=[64])