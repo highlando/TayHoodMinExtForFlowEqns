7d863dd3fd2e30440af535b3a94b1ef0d4407946
highlando <jnhlnd@gmail.com>
2015-01-05 11:47:02 +0100

    found triangle at Neumann border for cylwake

--- find_neumadj_cell.py
+++ find_neumadj_cell.py
@@ -1,12 +1,18 @@
 import dolfin
+import dolfin_navier_scipy.problem_setups as dnsps
 
 N = 10
 mesh = dolfin.UnitSquareMesh(N, N)
 
 edgepoint = dolfin.Point(1., 1.)
-
 aaa = mesh.bounding_box_tree().compute_first_entity_collision(edgepoint)
 
-dolfin.plot(mesh)
+femp, stokesmatsc, rhsd_vfrc, \
+    rhsd_stbc, data_prfx, ddir, proutdir \
+    = dnsps.get_sysmats(problem='cylinderwake', N=2, Re=2, scheme='CR')
 
+mesh = femp['mesh']
+edgepoint = dolfin.Point(2.2, .41)
+aaac = mesh.bounding_box_tree().compute_first_entity_collision(edgepoint)
+dolfin.plot(mesh)
 dolfin.interactive(True)
