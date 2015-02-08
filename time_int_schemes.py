eea00bb4bf16f47e278765cf3b753f2224713b55
highlando <jnhlnd@gmail.com>
2015-02-08 17:39:08 +0100

    minv prod and tolcor to string for ind2

--- time_int_schemes.py
+++ time_int_schemes.py
@@ -388,6 +388,7 @@ def halfexp_euler_nseind2(Mc, MP, Ac, BTc, Bc, fvbc, fpbc, PrP, TsP,
     PFacI = -1./dt
 
     dtstrdct = dict(prefix=TsP.svdatapath, method=2, N=PrP.N,
+                    tolcor=TsP.TolCorB,
                     nu=PrP.nu, Nts=TsP.Nts, tol=TsP.linatol, te=TsP.tE)
     cdatstr = get_dtstr(t=0, **dtstrdct)
     try:
@@ -447,7 +448,7 @@ def halfexp_euler_nseind2(Mc, MP, Ac, BTc, Bc, fvbc, fpbc, PrP, TsP,
         """
         v1, v2 = vp1[:Nv, ], vp2[:Nv, ]
         p1, p2 = vp1[Nv:, ], vp2[Nv:, ]
-        return mass_fem_ip(v1, v2, Mc) + mass_fem_ip(p1, p2, MPc)
+        return mass_fem_ip(v1, v2, Mcfac) + mass_fem_ip(p1, p2, MPcfac)
 
     inikryupd = TsP.inikryupd
     iniiterfac = TsP.iniiterfac  # the first krylov step needs more maxiter
