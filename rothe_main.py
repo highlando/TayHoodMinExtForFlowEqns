e09899086349d44b4776b7e4f775e94a8ef3efb4
highlando <jnhlnd@gmail.com>
2015-09-05 13:53:00 +0200

    comp vp error is really buggy

--- rothe_main.py
+++ rothe_main.py
@@ -3,7 +3,9 @@ import numpy as np
 
 import dolfin_navier_scipy.stokes_navier_utils as snu
 import dolfin_navier_scipy.data_output_utils as dou
+import dolfin_navier_scipy.dolfin_to_sparrays as dts
 import rothe_utils as rtu
+from time_int_schemes import get_dtstr
 
 dolfin.parameters.linear_algebra_backend = 'uBLAS'
 
@@ -35,38 +37,102 @@ Nplt = 2
 proutdir = 'results/'
 ddir = 'data/'
 
+debug = True
 debug = False
 
 
-def check_the_sim(problem='cylinderwake', index=2, nswtchl=[2], scheme='CR',
+def check_the_sim(problem='cylinderwake', index=2, nswtchsl=[2], scheme='CR',
                   nu=None, Re=None, Nts=None, paraout=False, t0=None, tE=None,
                   dtstrdct={}, debug=False):
 
-    refvdict, refpdict = gettheref(problem=problem, N=Nref, nu=nu, Re=Re,
-                                   t0=t0, tE=tE, Nts=Nts,
-                                   scheme=scheme, dtstrdct=dtstrdct,
-                                   debug=debug)
-    viniv = dou.load_npa(refvdict[t0])
-    piniv = dou.load_npa(refpdict[t0])
+    trange = np.linspace(t0, tE, Nts+1)
+    refdtstrdct = dict(prefix=ddir+problem+scheme,
+                       method=2, N=None, Nts=Nts, t0=t0, te=tE)
+    refdatstr = get_dtstr(t=None, **refdtstrdct)
+    compargs = dict(problem=problem, N=Nref, nu=nu, Re=Re, trange=trange,
+                    scheme=scheme, data_prefix=refdatstr, debug=debug)
+    refvdict, refpdict = dou.load_or_comp(filestr=[refdatstr+'_refvdict',
+                                                   refdatstr+'_refpdict'],
+                                          comprtn=gettheref,
+                                          comprtnargs=compargs,
+                                          debug=debug, itsadict=True)
+
+    viniv = dou.load_npa(refvdict['{0}'.format(t0)])
+    piniv = dou.load_npa(refpdict['{0}'.format(t0)])
+
+    # set up the list of the mesh parameters at every time step
+    swl = Nts/len(nswtchsl)
+    Nll = np.array([nswtchsl[0]]*(Nts+1))
+    for k, N in enumerate(nswtchsl[1:]):
+        Nll[(k+1)*swl+1:] = N
+    Nll[0] = Nref
+    Nll = Nll.tolist()
 
     vdict, pdict = rtu.rothe_ind2(problem=problem, scheme=scheme, Re=Re, nu=nu,
                                   t0=t0, tE=tE, Nts=Nts,
-                                  nswtchl=nswtchl, Nini=Nref,
-                                  dtstrdct=dtstrdct)
+                                  viniv=viniv, piniv=piniv, Nini=Nref,
+                                  Nll=Nll, dtstrdct=dtstrdct)
+
+    compvperrargs = dict(problem=problem, scheme=scheme,
+                         trange=trange.tolist(),
+                         rvd=refvdict, rpd=refpdict, Nref=Nref,
+                         cvd=vdict, cpd=pdict, Nll=Nll)
+
+    dtstrdct.update(t=None)
+    cdatstr = get_dtstr(**dtstrdct)
+    errdict = dou.load_or_comp(filestr=[cdatstr+'_vperrdict'],
+                               comprtn=compvperr, debug=debug,
+                               comprtnargs=compvperrargs, itsadict=True)
+
+    rtu.plottimeerrs(trange=trange, perrl=[errdict['perrl']],
+                     verrl=[errdict['verrl']], showplot=True)
+
+
+def compvperr(problem=None, scheme=None, trange=None,
+              rvd=None, rpd=None, Nref=None,
+              cvd=None, cpd=None, Nll=None):
+    verrl, perrl = [], []
+
+    rmd = rtu.get_curmeshdict(problem=problem, N=Nref,
+                              scheme=scheme, onlymesh=True)
+    for tk, t in enumerate(trange):
+        vref = dou.load_npa(rvd['{0}'.format(t)])
+        pref = dou.load_npa(rpd['{0}'.format(t)])
+        vreff, preff = dts.\
+            expand_vp_dolfunc(vc=vref, V=rmd['V'], pc=pref, Q=rmd['Q'])
+
+        cmd = rtu.get_curmeshdict(problem=problem, N=Nll[tk],
+                                  scheme=scheme, onlymesh=True)
+        try:
+            vcur = dou.load_npa(cvd[t])
+            pcur = dou.load_npa(cpd[t])
+        except KeyError:
+            vcur = dou.load_npa(cvd['{0}'.format(t)])
+            pcur = dou.load_npa(cpd['{0}'.format(t)])
+
+        raise Warning('TODO: debug')
+        vcurf, pcurf = dts.\
+            expand_vp_dolfunc(vc=vcur, V=cmd['V'], pc=pref, Q=rmd['Q'])
+        # vcurf, pcurf = dts.\
+        #     expand_vp_dolfunc(vc=vref, V=cmd['V'], pc=pcur, Q=cmd['Q'])
+        verrl.append(dolfin.errornorm(vreff, vcurf))
+        perrl.append(dolfin.errornorm(preff, pcurf))
+
+    return dict(verrl=verrl, perrl=perrl, trange=trange, Nll=Nll, Nref=Nref)
 
 
 def gettheref(problem='cylinderwake', N=None, nu=None, Re=None, Nts=None,
-              paraout=False, t0=None, tE=None, scheme=None, dtstrdct={},
+              paraout=False, trange=None, scheme=None, data_prefix='',
               debug=False):
-    trange = np.linspace(t0, tE, Nts+1)
     refmeshdict = rtu.get_curmeshdict(problem=problem, N=N, nu=nu,
                                       Re=Re, scheme=scheme)
 
     refvdict, refpdict = snu.\
-        solve_nse(trange=trange, clearprvdata=debug, data_prfx=ddir,
+        solve_nse(trange=trange, clearprvdata=debug, data_prfx=data_prefix,
                   vfileprfx=proutdir, pfileprfx=proutdir,
                   output_includes_bcs=True,
                   return_dictofvelstrs=True, return_dictofpstrs=True,
+                  dictkeysstr=True,
                   start_ssstokes=True, **refmeshdict)
 
     return refvdict, refpdict
@@ -75,9 +141,12 @@ def gettheref(problem='cylinderwake', N=None, nu=None, Re=None, Nts=None,
 if __name__ == '__main__':
     problem = 'cylinderwake'
     scheme = 'CR'
-    t0, tE, Nts = 0.0, 0.2, 128
-    nswtchl = [3, 2]
+    t0, tE, Nts = 0.0, 0.02, 16
+    nswtchshortlist = [3, 2, 3]  # we recommend to start with `Nref`
 
-    nswtchstr = 'Nswitches' + ''.join(str(e) for e in nswtchl)
+    nswtchstr = 'Nswitches' + ''.join(str(e) for e in nswtchshortlist)
     dtstrdct = dict(prefix=ddir+problem+scheme+nswtchstr,
                     method=2, N=None, Nts=Nts, t0=t0, te=tE)
+    check_the_sim(problem='cylinderwake', index=2, nswtchsl=nswtchshortlist,
+                  scheme='CR', Re=120, Nts=Nts, paraout=False, t0=t0, tE=tE,
+                  dtstrdct=dtstrdct, debug=debug)
--- rothe_utils.py
+++ rothe_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 import dolfin_navier_scipy.dolfin_to_sparrays as dts
 import dolfin_navier_scipy.problem_setups as dnsps
@@ -9,25 +10,19 @@ from time_int_schemes import get_dtstr
 
 
 def rothe_ind2(problem='cylinderwake', nu=None, Re=None,
-               Nts=256, t0=0.0, tE=0.2, Nlist=[2],
+               Nts=256, t0=0.0, tE=0.2, Nll=[2],
                viniv=None, piniv=None, Nini=None,
                scheme=None, dtstrdct={}):
 
     trange = np.linspace(t0, tE, Nts+1)
-    # set up the list of the mesh parameters at every time step
-    swl = Nts/len(Nlist)
-    Nll = [Nlist[0]]*Nts
-    for N in Nlist[1:]:
-        Nll[N*swl+1:] = N
-    Nll[0] = Nini
 
     t = trange[0]
     dtstrdct.update(dict(t=0, N=Nll[0]))
     cdatstr = get_dtstr(**dtstrdct)
     dou.save_npa(viniv, cdatstr + '__vel')
-    curvdict = {0: cdatstr + '__vel'}
+    curvdict = {t: cdatstr + '__vel'}
     dou.save_npa(piniv, cdatstr + '__p')
-    curpdict = {0: cdatstr + '__p'}
+    curpdict = {t: cdatstr + '__p'}
 
     vprev = viniv
     curmeshdict = get_curmeshdict(problem=problem, N=Nll[0], nu=nu, Re=Re,
@@ -40,13 +35,13 @@ def rothe_ind2(problem='cylinderwake', nu=None, Re=None,
             roth_upd_ind2(vvec=vprev, cts=cts,
                           Vc=curmeshdict['V'], diribcsc=curmeshdict['diribcs'],
                           nmd=curmeshdict, returnalu=True)
-        dtstrdct.update(dict(t=t, N=N))
+        dtstrdct.update(dict(t=t, N=Nll[tk+1]))
         cdatstr = get_dtstr(**dtstrdct)
         vcur = dts.append_bcs_vec(vpcur[:Nvc], **curmeshdict)
         dou.save_npa(vcur, cdatstr+'__vel')
-        curvdict.update({t: cdatstr})
+        curvdict.update({t: cdatstr+'__vel'})
         dou.save_npa(vpcur[Nvc:, :], cdatstr+'__p')
-        curpdict.update({t: cdatstr})
+        curpdict.update({t: cdatstr+'__p'})
         curmeshdict.update(dict(coefalu=coefalu))
 
     return curvdict, curpdict
@@ -65,9 +60,11 @@ def roth_upd_ind2(vvec=None, cts=None, nu=None, Vc=None, diribcsc=None,
     """
 
     if not nmd['V'] == Vc:
+        raise Warning('TODO: debug')
         vvec = _vctovn(vvec=vvec, Vc=Vc, diribcs=diribcsc, nmd=nmd)
+    print 'hello'
 
-    mvvec = nmd['M']*vvec
+    mvvec = nmd['M']*vvec[nmd['invinds'], :]
     convvec = dts.get_convvec(u0_vec=vvec, V=nmd['V'],
                               diribcs=nmd['diribcs'],
                               invinds=nmd['invinds'])
@@ -95,15 +92,37 @@ def _vctovn(vvec=None, vfunc=None, Vc=None, Vn=None, diribcs=None):
     return vvec
 
 
-def get_curmeshdict(problem=None, N=None, Re=None, nu=None, scheme=None):
+def get_curmeshdict(problem=None, N=None, Re=None, nu=None, scheme=None,
+                    onlymesh=False):
 
-    femp, stokesmatsc, rhsd = dnsps.get_sysmats(problem=problem, N=N,
-                                                Re=Re, nu=nu, scheme=scheme,
-                                                mergerhs=True)
-    M, A, J = stokesmatsc['M'], stokesmatsc['A'], stokesmatsc['J']
-    V, Q = femp['V'], femp['Q']
-    invinds, diribcs = femp['invinds'],  femp['diribcs']
-    fv, fp = rhsd['fv'], rhsd['fp']
+    if onlymesh:
+        femp = dnsps.get_sysmats(problem=problem, N=N, scheme=scheme,
+                                 onlymesh=True)
+        return femp
+    else:
+        femp, stokesmatsc, rhsd = dnsps.\
+            get_sysmats(problem=problem, N=N, Re=Re, nu=nu,
+                        scheme=scheme, mergerhs=True)
+        M, A, J = stokesmatsc['M'], stokesmatsc['A'], stokesmatsc['J']
+        V, Q = femp['V'], femp['Q']
+        invinds, diribcs = femp['invinds'],  femp['diribcs']
+        fv, fp = rhsd['fv'], rhsd['fp']
 
-    return dict(M=M, A=A, J=J, V=V, Q=Q, invinds=invinds, diribcs=diribcs,
-                fv=fv, fp=fp, N=N, Re=femp['Re'])
+        return dict(M=M, A=A, J=J, V=V, Q=Q, invinds=invinds, diribcs=diribcs,
+                    fv=fv, fp=fp, N=N, Re=femp['Re'])
+
+
+def plottimeerrs(trange=None, verrl=None, perrl=None, fignums=[131, 132],
+                 showplot=False):
+    if verrl is not None:
+        plt.figure(fignums[0])
+        for verr in verrl:
+            plt.plot(trange, verr)
+        plt.title('v error')
+    if perrl is not None:
+        plt.figure(fignums[1])
+        for perr in perrl:
+            plt.plot(trange, perr)
+        plt.title('p error')
+    if showplot:
+        plt.show(block=False)
